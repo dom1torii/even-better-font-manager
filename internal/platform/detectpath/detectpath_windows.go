3718e4b0838767d4b11dfb3827cad51f923c3596
dom1torii <contact@domitori.xyz>
2026-02-15 23:42:41 +0200

    cs2 path detection for windows

--- internal/platform/detectpath/detectpath_windows.go
+++ internal/platform/detectpath/detectpath_windows.go
@@ -2,6 +2,24 @@
 
 package detectpath
 
+import (
+	"log"
+
+	"golang.org/x/sys/windows/registry"
+)
+
+// not tested yet, too lazy to open windows vm, surely it works
 func DetectCS2Path() string {
-	return ""
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\WOW6432Node\Valve\cs2`, registry.QUERY_VALUE)
+	if err != nil {
+		log.Println("No CS2 path detected")
+	}
+	defer k.Close()
+
+	s, _, err := k.GetStringValue("installpath")
+	if err != nil {
+		log.Println("No CS2 path detected")
+	}
+
+	return s
 }
